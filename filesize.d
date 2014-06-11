9e0216581469c1edf67f7948b62aea36c4bc66c3
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2014-06-10 19:20:12 -1000

    fixed ~ in writeln to simply seperate arguments

--- filesize.d
+++ filesize.d
@@ -1,15 +1,16 @@
 void main(string[] argv){
     import std.stdio, std.file, std.container, std.conv, std.algorithm;
 
+    assert(argv.length == 3);
     DirEntry[] arr;
     arr.length = to!uint(argv[2]);
-    auto heap = BinaryHeap!(DirEntry[], "a.size > b.size")(arr, 0);
+    auto heap = BinaryHeap!(typeof(arr), "a.size > b.size")(arr, 0);
 
     foreach(DirEntry e; dirEntries(argv[1], SpanMode.depth))
         if(!e.isSymlink)
             heap.conditionalInsert(e);
 
     foreach (e; sort!"a.size > b.size"(heap.release))
-        writeln(e.name ~ " " ~ to!string(e.size));
+        writeln(e.name, " " , e.size);
 
 }
